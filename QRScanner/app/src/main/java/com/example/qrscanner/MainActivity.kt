24073d772c12992968947a9490053c14cd7c7378
adrianprzcb <adrianpc4444@gmail.com>
2024-11-15 00:05:35 +0100

    new

--- QRScanner/app/src/main/java/com/example/qrscanner/MainActivity.kt
+++ QRScanner/app/src/main/java/com/example/qrscanner/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.text.HtmlCompat.FROM_HTML_MODE_LEGACY
+import androidx.core.text.HtmlCompat.fromHtml
 import com.example.qrscanner.databinding.ActivityMainBinding
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
@@ -11,6 +13,8 @@ import com.google.zxing.integration.android.IntentResult
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var qrLink : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -37,10 +41,14 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
             }else{
                 Toast.makeText(this, "El valor escaneado es:${result.contents} ", Toast.LENGTH_LONG).show()
-
+                qrLink = result.contents
             }
         }else{
             super.onActivityResult(requestCode, resultCode, data)
         }
+
+        var linkhtml = "<a href='$qrLink'> $qrLink </a>"
+        binding.tvResult.setClickable(true);
+        binding.tvResult.text = fromHtml(linkhtml, FROM_HTML_MODE_LEGACY)
     }
 }
