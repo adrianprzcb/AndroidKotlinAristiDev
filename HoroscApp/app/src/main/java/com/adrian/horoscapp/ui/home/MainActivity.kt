47870e7937cc39e4585b92d9e8179be20e93f6b0
adrianprzcb <adrianpc4444@gmail.com>
2024-11-15 01:34:07 +0100

    new

--- HoroscApp/app/src/main/java/com/adrian/horoscapp/ui/home/MainActivity.kt
+++ HoroscApp/app/src/main/java/com/adrian/horoscapp/ui/home/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.adrian.horoscapp
+package com.adrian.horoscapp.ui.home
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.adrian.horoscapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
