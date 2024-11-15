557833f0496e92de19abe831689097d86007c116
adrianprzcb <adrianpc4444@gmail.com>
2024-11-15 13:19:14 +0100

    new

---
+++ HoroscApp/app/src/main/java/com/adrian/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,26 @@
+package com.adrian.horoscapp.ui.horoscope
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.adrian.horoscapp.R
+import com.adrian.horoscapp.databinding.FragmentHoroscopeBinding
+
+
+class HoroscopeFragment : Fragment() {
+
+    private var _binding: FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHoroscopeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
