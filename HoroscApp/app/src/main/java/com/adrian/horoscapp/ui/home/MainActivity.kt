d593a5115957d5c22ff5b9c78c129201dbac5c72
adrianprzcb <adrianpc4444@gmail.com>
2024-12-04 18:23:00 +0100

    added toolbar

--- HoroscApp/app/src/main/java/com/adrian/horoscapp/ui/home/MainActivity.kt
+++ HoroscApp/app/src/main/java/com/adrian/horoscapp/ui/home/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.adrian.horoscapp.R
 import com.adrian.horoscapp.databinding.ActivityMainBinding
 
@@ -12,10 +15,22 @@ class MainActivity : AppCompatActivity() {
 
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUi()
+    }
+
+    private fun initUi() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+       val navHost= supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
