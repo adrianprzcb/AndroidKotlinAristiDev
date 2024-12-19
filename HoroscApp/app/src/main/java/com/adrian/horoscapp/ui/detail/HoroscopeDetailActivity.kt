5fbf9c5e0ff8e5bfefe51d35383efdfa6b35285b
adrianprzcb <adrianpc4444@gmail.com>
2024-12-19 11:40:08 +0100

    updt

--- HoroscApp/app/src/main/java/com/adrian/horoscapp/ui/detail/HoroscopeDetailActivity.kt
+++ HoroscApp/app/src/main/java/com/adrian/horoscapp/ui/detail/HoroscopeDetailActivity.kt
@@ -3,18 +3,23 @@ package com.adrian.horoscapp.ui.detail
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavArgs
 import androidx.navigation.navArgs
 import com.adrian.horoscapp.databinding.ActivityHoroscopeDetailBinding
 import com.adrian.horoscapp.ui.horoscope.HoroscopeViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
 
 
     private lateinit var binding: ActivityHoroscopeDetailBinding
-    private val horoscopeDetailViewModel: HoroscopeViewModel by viewModels()
+    private val horoscopeDetailViewModel: HoroscopeDetailViewModel by viewModels()
 
     private val args:HoroscopeDetailActivityArgs by navArgs()
 
@@ -22,6 +27,37 @@ class HoroscopeDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        args.type
+        initUi()
+    }
+
+    private fun initUi() {
+        initUiState()
+    }
+
+    private fun initUiState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                horoscopeDetailViewModel.state.collect{
+                    when(it){
+                        HoroscopeDetailState.Loading -> loadingState()
+                        is HoroscopeDetailState.Error -> errorState()
+                        is HoroscopeDetailState.Success -> successState()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun successState() {
+        TODO("Not yet implemented")
+    }
+
+    private fun errorState() {
+        TODO("Not yet implemented")
+    }
+
+
+    private fun loadingState(){
+        binding.pb.isVisible = true
     }
 }
