0342999607cf9baea7253c9c63df1c0dc5b3dc64
adrianprzcb <adrianpc4444@gmail.com>
2025-01-15 17:58:06 +0100

    updt

--- HoroscApp/app/src/main/java/com/adrian/horoscapp/ui/luck/LuckFragment.kt
+++ HoroscApp/app/src/main/java/com/adrian/horoscapp/ui/luck/LuckFragment.kt
@@ -1,13 +1,17 @@
 package com.adrian.horoscapp.ui.luck
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
 import com.adrian.horoscapp.R
 import com.adrian.horoscapp.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Random
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +24,33 @@ class LuckFragment : Fragment() {
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUi()
+    }
+
+    private fun initUi(){
+        initListeners()
+    }
+
+    private fun initListeners(){
+        binding.ivRoulette.setOnClickListener{ spinRoulette() }
+    }
+
+    private fun spinRoulette(){
+        val random = Random()
+        val degrees = random.nextInt(1440) + 360
+        val animator = ObjectAnimator.ofFloat(binding.ivRoulette, View.ROTATION , 0f, degrees.toFloat())
+        animator.duration = 2000
+        animator.interpolator = DecelerateInterpolator()
+        animator.doOnEnd {  }
+        animator.start()
+    }
+
+    private fun slideCard(){
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
