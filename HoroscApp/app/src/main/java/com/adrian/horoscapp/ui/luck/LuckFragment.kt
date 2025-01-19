4abfec54e7bb5ad0d80829da8c437245c38e66c1
adrianprzcb <adrianpc4444@gmail.com>
2025-01-19 22:53:59 +0100

    updt

--- HoroscApp/app/src/main/java/com/adrian/horoscapp/ui/luck/LuckFragment.kt
+++ HoroscApp/app/src/main/java/com/adrian/horoscapp/ui/luck/LuckFragment.kt
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.view.animation.DecelerateInterpolator
 import androidx.core.animation.doOnEnd
+import androidx.core.view.isVisible
 import com.adrian.horoscapp.R
 import com.adrian.horoscapp.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -46,12 +48,31 @@ class LuckFragment : Fragment() {
         val animator = ObjectAnimator.ofFloat(binding.ivRoulette, View.ROTATION , 0f, degrees.toFloat())
         animator.duration = 2000
         animator.interpolator = DecelerateInterpolator()
-        animator.doOnEnd {  }
+        animator.doOnEnd { slideCard() }
         animator.start()
     }
 
     private fun slideCard(){
-        val slideUpAnimation = AnimationUtils.loadAnimation(requireContext(), )
+        val slideUpAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_up )
+
+        slideUpAnimation.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationStart(p0: Animation?) {
+                binding.reverse.isVisible = true
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                growCard()
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {}
+
+        })
+
+        binding.reverse.startAnimation(slideUpAnimation)
+    }
+
+    private fun growCard(){
+
     }
 
 
