d1b05db0af30be5e0fa59b6683e16035ae20a44d
adrianprzcb <adrianpc4444@gmail.com>
2025-01-27 22:08:46 +0100

    updt

--- HoroscApp/app/build.gradle.kts
+++ HoroscApp/app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
 
     //NavComponent
     val navVersion = "2.7.1"
+    val cameraVersion = "1.2.3"
 
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
@@ -79,5 +80,11 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.3.1")
 
+    //Camera X
+    implementation ("androidx.camera:camera-core:${cameraVersion}")
+    implementation ("androidx.camera:camera-camera2:${cameraVersion}")
+    implementation ("androidx.camera:camera-lifecycle:${cameraVersion}")
+    implementation ("androidx.camera:camera-view:${cameraVersion}")
+    implementation ("androidx.camera:camera-extensions:${cameraVersion}")
 
 }
