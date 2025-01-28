d7a850d94a79051da9d2c9656561965ab4cf3691
adrianprzcb <adrianpc4444@gmail.com>
2025-01-28 12:54:39 +0100

    updt

--- HoroscApp/app/src/main/java/com/adrian/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ HoroscApp/app/src/main/java/com/adrian/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.PermissionChecker
 import com.adrian.horoscapp.R
 import com.adrian.horoscapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,8 +13,29 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
 
+    companion object {
+        private const val CAMERA_PERMISSION = android.Manifest.permission.CAMERA
+    }
+
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(checkCameraPermission()){
+
+        }else{
+
+        }
+    }
+
+    private fun checkCameraPermission(): Boolean {
+        return PermissionChecker.checkSelfPermission(
+            requireContext(), CAMERA_PERMISSION
+        ) == PermissionChecker.PERMISSION_GRANTED
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
